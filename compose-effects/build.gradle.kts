c2b2019ff9f1bd3bfa2b282aa47a504467a03fb7
skydoves <skydoves@naver.com>
2025-03-10 16:25:42 +0900

    Initial commit

---
+++ compose-effects/build.gradle.kts
@@ -0,0 +1,110 @@
+/*
+ * Designed and developed by 2025 skydoves (Jaewoong Eum)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import com.skydoves.compose.effects.Configuration
+
+plugins {
+  id(libs.plugins.android.library.get().pluginId)
+  id(libs.plugins.kotlin.multiplatform.get().pluginId)
+  id(libs.plugins.compose.compiler.get().pluginId)
+  id(libs.plugins.jetbrains.compose.get().pluginId)
+  id(libs.plugins.nexus.plugin.get().pluginId)
+}
+
+apply(from = "${rootDir}/scripts/publish-module.gradle.kts")
+
+mavenPublishing {
+  val artifactId = "compose-effects"
+  coordinates(
+    Configuration.artifactGroup,
+    artifactId,
+    rootProject.extra.get("libVersion").toString()
+  )
+
+  pom {
+    name.set(artifactId)
+    description.set("Compose Effects enable you to launch efficient side-effects without unnecessary operations.")
+  }
+}
+
+kotlin {
+  listOf(
+    iosX64(),
+    iosArm64(),
+    iosSimulatorArm64(),
+    macosArm64(),
+    macosX64(),
+  ).forEach {
+    it.binaries.framework {
+      baseName = "common"
+    }
+  }
+
+  androidTarget {
+    publishLibraryVariants("release")
+  }
+
+  jvm {
+    libs.versions.jvmTarget.get().toInt()
+    compilations.all {
+      kotlinOptions.jvmTarget = libs.versions.jvmTarget.get()
+    }
+  }
+
+  applyDefaultHierarchyTemplate()
+
+  sourceSets {
+    val commonMain by getting {
+      dependencies {
+        implementation(compose.runtime)
+        implementation(compose.foundation)
+        implementation(compose.ui)
+      }
+    }
+  }
+
+  explicitApi()
+
+  sourceSets.androidInstrumentedTest.dependencies {
+    implementation(kotlin("test"))
+  }
+}
+
+android {
+  compileSdk = Configuration.compileSdk
+  namespace = "com.skydoves.compose.effects"
+
+  defaultConfig {
+    minSdk = Configuration.minSdk
+    testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+  }
+
+  compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+  }
+}
+
+dependencies {
+  debugImplementation(libs.androidx.ui.test.manifest)
+  androidTestImplementation(platform(libs.androidx.compose.bom))
+  androidTestImplementation(libs.androidx.compose.material3)
+  androidTestImplementation(libs.androidx.activity.compose)
+  androidTestImplementation(libs.androidx.test.rules)
+  androidTestImplementation(libs.androidx.test.runner)
+  androidTestImplementation(libs.androidx.test.junit)
+  androidTestImplementation(libs.androidx.compose.ui)
+  androidTestImplementation(libs.androidx.compose.ui.test)
+}
